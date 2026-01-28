ee6b270b611d4ff737c9d64fc67717cb57713312
theharshkumar-bits <harshkumar532003@gmail.com>
2026-01-28 12:12:43 +0530

    synchronization added to refresh method of RulCache

--- src/main/java/com/example/decision_engine/cache/RuleCache.java
+++ src/main/java/com/example/decision_engine/cache/RuleCache.java
@@ -17,12 +17,20 @@ public class RuleCache {
 
     @PostConstruct   //Load Rules at startup
     private void setCache(){
-        cache = ruleRepository.findAll();
-        System.out.println("rule cached : "+cache.size());
+//        cache = ruleRepository.findAll();
+//        System.out.println("rule cached : "+cache.size());
+        refresh();
     }
 
-    public void refresh(){
+//    public void refresh(){
+        //after adding CacheController , i added synchronized keyword to avoid thread condition to avoid unpredictable result ex only 5 result updated or 10 rule updated means not fully written list it can be half written list not with complete rules
+        //Worst case:
+        //One request uses slightly outdated rules (old cache) for a moment.
+        //That is acceptable in most caching systems.
+        //no need now for ReadWriteLock
+        public synchronized void refresh(){
         cache=ruleRepository.findAll();
+        System.out.println("Rule cache refresh and size = "+cache.size());
     }
     public List<RuleEntity> getCache(){
         return cache;
